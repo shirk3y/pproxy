f4427d921dfa319703bce3552096fb14c8af1275
Hong Minhee <minhee@dahlia.kr>
2013-04-03 19:51:31 +0900

    Fix the upstream issue 1:
    http://code.google.com/p/wsgi-proxy/issues/detail?id=1

--- wsgi_proxy/__init__.py
+++ wsgi_proxy/__init__.py
@@ -21,19 +21,7 @@ def is_hop_by_hop(header):
 
 def reconstruct_url(environ):
     # From WSGI spec, PEP 333
-    url = environ['wsgi.url_scheme'] + '://'
-    if environ.get('HTTP_HOST'):
-        url += environ['HTTP_HOST']
-    else:
-        url += environ['SERVER_NAME']
-        if environ['wsgi.url_scheme'] == 'https':
-            if environ['SERVER_PORT'] != '443':
-               url += ':' + environ['SERVER_PORT']
-        else:
-            if environ['SERVER_PORT'] != '80':
-               url += ':' + environ['SERVER_PORT']
-    url += environ.get('SCRIPT_NAME', '')
-    url += environ.get('PATH_INFO', '')
+    url = environ.get('PATH_INFO', '')
     # Fix ;arg=value in url
     if url.find('%3B') is not -1:
         url, arg = url.split('%3B', 1)
