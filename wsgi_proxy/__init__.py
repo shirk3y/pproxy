78a927d65fee89f5c2f064dd6db6402e1893ac99
Hong Minhee <minhee@dahlia.kr>
2013-04-03 19:52:02 +0900

    Make more idiomatic

--- wsgi_proxy/__init__.py
+++ wsgi_proxy/__init__.py
@@ -2,33 +2,36 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
-import copy
 import httplib
 import logging
 import urlparse
 
-logger = logging.getLogger(__name__)
 
-_hoppish = {
-    'connection': 1, 'keep-alive': 1, 'proxy-authenticate': 1,
-    'proxy-authorization': 1, 'te': 1, 'trailers': 1, 'transfer-encoding': 1,
-    'upgrade': 1, 'proxy-connection': 1 }
+HOPPISH_HEADERS = frozenset([
+    'connection', 'keep-alive', 'proxy-authenticate',
+    'proxy-authorization', 'te', 'trailers', 'transfer-encoding',
+    'upgrade', 'proxy-connection'
+])
 
 
 def is_hop_by_hop(header):
-    return _hoppish.has_key(header.lower())
+    return header.lower() in HOPPISH_HEADERS
 
 
 def reconstruct_url(environ):
     # From WSGI spec, PEP 333
     url = environ.get('PATH_INFO', '')
     # Fix ;arg=value in url
-    if url.find('%3B') is not -1:
+    if '%3B' in url:
         url, arg = url.split('%3B', 1)
         url = ';'.join([url, arg.replace('%3D', '=')])
     # Stick query string back in
-    if environ.get('QUERY_STRING'):
-        url += '?' + environ['QUERY_STRING']
+    try:
+        query_string = environ['QUERY_STRING']
+    except KeyError:
+        pass
+    else:
+        url += '?' + query_string
     environ['reconstructed_url'] = url
     return url
 
@@ -36,75 +39,86 @@ def reconstruct_url(environ):
 class WSGIProxyApplication(object):
     """Application to handle requests that need to be proxied"""
 
-    ConnectionClass = httplib.HTTPConnection
+    connection_class = httplib.HTTPConnection
 
     def handler(self, environ, start_response):
         """Proxy for requests to the actual http server"""
+        logger = logging.getLogger(__name__ + '.WSGIProxyApplication.handler')
         url = urlparse.urlparse(reconstruct_url(environ))
 
         # Create connection object
         try:
-            connection = self.ConnectionClass(url.netloc)
+            connection = self.connection_class(url.netloc)
             # Build path
             path = url.geturl().replace('%s://%s' % (url.scheme, url.netloc),
                                         '')
         except Exception:
-            start_response("501 Gateway error", [('Content-Type', 'text/html')])
+            start_response('501 Gateway Error', [('Content-Type', 'text/html')])
             logger.exception('Could not Connect')
-            return ['<H1>Could not connect</H1>']
+            yield '<H1>Could not connect</H1>'
+            return
 
         # Read in request body if it exists
         body = None
-        if environ.get('CONTENT_LENGTH'):
+        try:
             length = int(environ['CONTENT_LENGTH'])
+        except (KeyError, ValueError):
+            pass
+        else:
             body = environ['wsgi.input'].read(length)
 
         # Build headers
-        headers = {}
-        logger.debug('Environ ; %s' % str(environ))
-        for key in environ.keys():
-            # Keys that start with HTTP_ are all headers
-            if key.startswith('HTTP_'):
+        logger.debug('environ = %r', environ)
+        headers = dict(
+            (key, value)
+            for key, value in (
                 # This is a hacky way of getting the header names right
-                value = environ[key]
-                key = key.replace('HTTP_', '', 1).swapcase().replace('_', '-')
-                if is_hop_by_hop(key) is False:
-                    headers[key] = value
+                (key[5:].lower().replace('_', '-'), value)
+                for key, value in environ.items()
+                # Keys that start with HTTP_ are all headers
+                if key.startswith('HTTP_')
+            )
+            if not is_hop_by_hop(key)
+        )
 
         # Handler headers that aren't HTTP_ in environ
-        if environ.get('CONTENT_TYPE'):
+        try:
             headers['content-type'] = environ['CONTENT_TYPE']
+        except KeyError:
+            pass
 
         # Add our host if one isn't defined
-        if not headers.has_key('host'):
+        if 'host' not in headers:
             headers['host'] = environ['SERVER_NAME']
 
         # Make the remote request
         try:
-            logger.debug(
-                '%s %s %s' %
-                (environ['REQUEST_METHOD'], path, str(headers))
-            )
+            logger.debug('%s %s %r',
+                         environ['REQUEST_METHOD'], path, headers)
             connection.request(environ['REQUEST_METHOD'], path,
                                body=body, headers=headers)
-        except:
+        except Exception as e:
             # We need extra exception handling in the case the server fails
             # in mid connection, it's an edge case but I've seen it
-            start_response("501 Gateway error", [('Content-Type', 'text/html')])
-            logger.exception('Could not Connect')
-            return ['<H1>Could not connect</H1>']
+            start_response('501 Gateway Error', [('Content-Type', 'text/html')])
+            logger.exception(e)
+            yield '<H1>Could not connect</H1>'
+            return
 
         response = connection.getresponse()
 
         hopped_headers = response.getheaders()
-        headers = copy.copy(hopped_headers)
-        for header in hopped_headers:
-            if is_hop_by_hop(header[0]):
-                headers.remove(header)
+        headers = [(key, value)
+                   for key, value in hopped_headers
+                   if not is_hop_by_hop(key)]
 
-        start_response(response.status.__str__() + ' ' + response.reason,
-                       headers)
-        return [response.read(response.length)]
+        start_response('{0.status} {0.reason}'.format(response), headers)
+        while True:
+            chunk = response.read(4096)
+            if chunk:
+                yield chunk
+            else:
+                break
 
     def __call__(self, environ, start_response):
         return self.handler(environ, start_response)
