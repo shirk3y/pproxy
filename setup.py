fb5c387fc3b6f76a61d74f54c6bd59cd82bacd58
Hong Minhee <minhee@dahlia.kr>
2013-04-03 19:25:32 +0900

    Coding style

--- setup.py
+++ setup.py
@@ -2,23 +2,25 @@ from setuptools import setup, find_packages
 
 from wsgi_proxy.version import VERSION
 
-setup(name='wsgi-prixy',
-      version=VERSION,
-      description='WSGI proxy application',
-      author='OSAF, Mikeal Rogers',
-      author_email='mikeal.rogers' '@' 'gmail.com',
-      maintainer='Hong Minhee',
-      maintainer_email='minhee' '@' 'dahlia.kr',
-      url='http://code.google.com/p/wsgi-proxy/',
-      license='Apache License 2.0',
-      packages=find_packages(),
-      platforms=['Any'],
-      classifiers=[
-      'Development Status :: 4 - Beta',
-                   'Environment :: Console',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: Apache Software License',
-                   'Operating System :: OS Independent',
-                   'Topic :: Software Development :: Libraries :: Python Modules',
-                  ]
-     )
+
+setup(
+    name='wsgi-prixy',
+    version=VERSION,
+    description='WSGI proxy application',
+    author='OSAF, Mikeal Rogers',
+    author_email='mikeal.rogers' '@' 'gmail.com',
+    maintainer='Hong Minhee',
+    maintainer_email='minhee' '@' 'dahlia.kr',
+    url='http://code.google.com/p/wsgi-proxy/',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    platforms=['Any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
--- wsgi_proxy/__init__.py
+++ wsgi_proxy/__init__.py
@@ -1,22 +1,29 @@
-from httplib import HTTPConnection
-from urlparse import urlparse
+""":mod:`wsgi_proxy` --- WSGI proxy app
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
 import copy
+import httplib
 import logging
+import urlparse
 
 logger = logging.getLogger(__name__)
 
 _hoppish = {
-    'connection':1, 'keep-alive':1, 'proxy-authenticate':1,
-    'proxy-authorization':1, 'te':1, 'trailers':1, 'transfer-encoding':1,
-    'upgrade':1, 'proxy-connection':1 }
-    
+    'connection': 1, 'keep-alive': 1, 'proxy-authenticate': 1,
+    'proxy-authorization': 1, 'te': 1, 'trailers': 1, 'transfer-encoding': 1,
+    'upgrade': 1, 'proxy-connection': 1 }
+
+
 def is_hop_by_hop(header):
     return _hoppish.has_key(header.lower())
 
+
 def reconstruct_url(environ):
     # From WSGI spec, PEP 333
-    url = environ['wsgi.url_scheme']+'://'
-    if environ.get('HTTP_HOST'): url += environ['HTTP_HOST']
+    url = environ['wsgi.url_scheme'] + '://'
+    if environ.get('HTTP_HOST'):
+        url += environ['HTTP_HOST']
     else:
         url += environ['SERVER_NAME']
         if environ['wsgi.url_scheme'] == 'https':
@@ -25,8 +32,8 @@ def reconstruct_url(environ):
         else:
             if environ['SERVER_PORT'] != '80':
                url += ':' + environ['SERVER_PORT']
-    url += environ.get('SCRIPT_NAME','')
-    url += environ.get('PATH_INFO','')
+    url += environ.get('SCRIPT_NAME', '')
+    url += environ.get('PATH_INFO', '')
     # Fix ;arg=value in url
     if url.find('%3B') is not -1:
         url, arg = url.split('%3B', 1)
@@ -34,37 +41,36 @@ def reconstruct_url(environ):
     # Stick query string back in
     if environ.get('QUERY_STRING'):
         url += '?' + environ['QUERY_STRING']
-
     environ['reconstructed_url'] = url
     return url
 
+
 class WSGIProxyApplication(object):
     """Application to handle requests that need to be proxied"""
-        
-    ConnectionClass = HTTPConnection
-    
+
+    ConnectionClass = httplib.HTTPConnection
+
     def handler(self, environ, start_response):
         """Proxy for requests to the actual http server"""
-        url = urlparse(reconstruct_url(environ))
-    
+        url = urlparse.urlparse(reconstruct_url(environ))
+
         # Create connection object
         try:
             connection = self.ConnectionClass(url.netloc)
             # Build path
-            path = url.geturl().replace('%s://%s' % (url.scheme, url.netloc), '')
-        except Exception, e:
+            path = url.geturl().replace('%s://%s' % (url.scheme, url.netloc),
+                                        '')
+        except Exception:
             start_response("501 Gateway error", [('Content-Type', 'text/html')])
             logger.exception('Could not Connect')
             return ['<H1>Could not connect</H1>']
 
-            
-        # Read in request body if it exists    
+        # Read in request body if it exists
         body = None
         if environ.get('CONTENT_LENGTH'):
             length = int(environ['CONTENT_LENGTH'])
             body = environ['wsgi.input'].read(length)
-            
-            
+
         # Build headers
         headers = {}
         logger.debug('Environ ; %s' % str(environ))
@@ -76,21 +82,26 @@ class WSGIProxyApplication(object):
                 key = key.replace('HTTP_', '', 1).swapcase().replace('_', '-')
                 if is_hop_by_hop(key) is False:
                     headers[key] = value
-    
+
         # Handler headers that aren't HTTP_ in environ
         if environ.get('CONTENT_TYPE'):
             headers['content-type'] = environ['CONTENT_TYPE']
-        
+
         # Add our host if one isn't defined
         if not headers.has_key('host'):
-            headers['host'] = environ['SERVER_NAME']   
-    
+            headers['host'] = environ['SERVER_NAME']
+
         # Make the remote request
         try:
-            logger.debug('%s %s %s' % (environ['REQUEST_METHOD'], path, str(headers)))
-            connection.request(environ['REQUEST_METHOD'], path, body=body, headers=headers)
+            logger.debug(
+                '%s %s %s' %
+                (environ['REQUEST_METHOD'], path, str(headers))
+            )
+            connection.request(environ['REQUEST_METHOD'], path,
+                               body=body, headers=headers)
         except:
-            # We need extra exception handling in the case the server fails in mid connection, it's an edge case but I've seen it
+            # We need extra exception handling in the case the server fails
+            # in mid connection, it's an edge case but I've seen it
             start_response("501 Gateway error", [('Content-Type', 'text/html')])
             logger.exception('Could not Connect')
             return ['<H1>Could not connect</H1>']
@@ -102,10 +113,10 @@ class WSGIProxyApplication(object):
         for header in hopped_headers:
             if is_hop_by_hop(header[0]):
                 headers.remove(header)
-        
-        start_response(response.status.__str__()+' '+response.reason, headers)
+
+        start_response(response.status.__str__() + ' ' + response.reason,
+                       headers)
         return [response.read(response.length)]
-        
-    
+
     def __call__(self, environ, start_response):
         return self.handler(environ, start_response)
