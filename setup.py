6fb1878156b0a52bd714c6ae498cd15531e96f4d
Hong Minhee <minhee@dahlia.kr>
2013-04-03 19:18:51 +0900

    Adjust package metadata

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@ from setuptools import setup, find_packages
 
 from wsgi_proxy.version import VERSION
 
-desc = """WSGI proxy application"""
-summ = """WSGI application for a simple HTTP 1.0 proxy."""
-
 setup(name='wsgi-prixy',
       version=VERSION,
       description='WSGI proxy application',
-      summary=summ,
       author='OSAF, Mikeal Rogers',
-      author_email='mikeal.rogers@gmail.com',
+      author_email='mikeal.rogers' '@' 'gmail.com',
+      maintainer='Hong Minhee',
+      maintainer_email='minhee' '@' 'dahlia.kr',
       url='http://code.google.com/p/wsgi-proxy/',
       license='Apache License 2.0',
       packages=find_packages(),
       platforms=['Any'],
-      classifiers=['Development Status :: 4 - Beta',
+      classifiers=[
+      'Development Status :: 4 - Beta',
                    'Environment :: Console',
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: Apache Software License',
